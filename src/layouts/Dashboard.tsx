9305af5e3bd3535be239f10c3667b35051d60d3b
yashashvi <yashashvimaurya@gmail.com>
2025-12-26 19:21:09 +0530

    Add new icons and enhance Dashboard layout with navigation links
    - Introduced multiple new icon components: BagIcon, BarChartIcon, BasketIcon, CategoryIcon, DashboardIcon, FoodIcon, GiftIcon, Home, LogoIcon, ToppingIcon, and UserIcon.
    - Updated Dashboard layout to include a sidebar with navigation links for Home, Users, Restaurants, Products, and Promos.
    - Enhanced styling for the login form and logo.
    - Integrated Ant Design components for improved UI consistency.

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,15 +1,115 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, NavLink, Outlet } from 'react-router-dom'
 import { useAuthStore } from '../store'
+import { Avatar, Dropdown, Layout, Menu, Space, theme } from 'antd';
+import { useState } from 'react';
+import Logo from '../components/icons/Logo';
+import Icon, { BellFilled } from '@ant-design/icons';
+import Home from '../components/icons/Home';
+import UserIcon from '../components/icons/UserIcon';
+import { foodIcon } from '../components/icons/FoodIcon';
+import GiftIcon from '../components/icons/GiftIcon';
+import BasketIcon from '../components/icons/BasketIcon';
+import { Badge, Button, Flex, Segmented } from 'antd';
+import type { FlexProps } from 'antd';
+import { useMutation } from '@tanstack/react-query';
+import { logout } from '../http/api';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [
+    {
+        key: '/',
+        icon:  <Icon component={Home} /> ,
+        label: <NavLink to="/" >Home</NavLink>
+    },
+    {
+        key: '/users',
+        icon: <Icon component={UserIcon} />,
+        label: <NavLink to="/users" >Users</NavLink>
+    },
+    {
+        key: '/restaurants',
+        icon: <Icon component={foodIcon} />,
+        label: <NavLink to="/restaurants" >Restaurants</NavLink>
+    },
+    {
+      key: '/products',
+      icon: <Icon component={BasketIcon} />,
+      label: <NavLink to="/products" >Products</NavLink>
+    },
+    {
+      key: '/promos',
+      icon: <Icon component={GiftIcon} />,
+      label: <NavLink to="/promos" >Promos</NavLink>
+    }
+]
 
 export default function Dashboard() {
+    const {logoutFromStore}  = useAuthStore();
+    const {mutate: logoutMutate} = useMutation({
+      mutationKey: ['logout'],
+      mutationFn: logout,
+      onSuccess: () => {
+        logoutFromStore();
+        return;
+      }
+    })
+    const [collapsed,setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+      } = theme.useToken();
     const {user} = useAuthStore();
     if(!user){
         return <Navigate to="/auth/login" replace={true} />
     }
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Outlet/>
-    </div>
+    <Layout style={{ minHeight: '100vh', background: colorBgContainer }}>
+      <Sider theme="light" collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo">
+            <Logo/>
+        </div>
+        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{ paddingLeft: '16px', paddingRight: '16px', background: colorBgContainer }}>
+        <Flex gap="middle" align="start" justify='space-between'>
+         <Badge text="Global" status="success" />
+         <Space size={20} >
+          <Badge dot={true} >
+            <BellFilled/>
+          </Badge>
+            <Dropdown menu={{ items: [
+              {
+                key: 'logout',
+                label: 'Logout',
+                onClick: () => logoutMutate()
+              }
+            ] }} placement='bottomRight' >
+              <Avatar
+                style={{backgroundColor: '#fde3df', color: '#f56a00'}}
+              >
+                U
+              </Avatar>
+            </Dropdown>
+         </Space>
+        </Flex>
+        </Header>
+        <Content style={{ margin: '0 16px' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet/>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Yashashvi Pizza app
+        </Footer>
+      </Layout>
+    </Layout>
   )
 }
