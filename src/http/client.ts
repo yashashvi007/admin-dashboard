015dd3eeb492d060e9c77eff25667363aa9fce7d
yashashvi <yashashvimaurya@gmail.com>
2025-12-26 19:21:17 +0530

    Add token refresh mechanism in Axios interceptor for improved authentication handling
    - Implemented a refreshToken function to handle 401 errors by attempting to refresh the authentication token.
    - Updated Axios response interceptor to retry the original request after a successful token refresh.
    - Integrated logout functionality in case of refresh token failure, utilizing the authentication store.

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_API_URL,
@@ -7,6 +8,31 @@ const api = axios.create({
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     }
+})
+
+const refreshToken = async () => {
+    const response = await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,{}, {
+        withCredentials: true,
+    });
+    return response.data;
+}
+
+api.interceptors.response.use((response) => response, async (error)=> {
+    const originalRequest = error.config;
+    if(error.response.status === 401 &&  !originalRequest._isRetry) {
+        try {
+            originalRequest._isRetry = true;
+            const headers = {...originalRequest.headers};
+            await refreshToken();
+            return api.request({...originalRequest, headers});
+        } catch (error) {
+            console.error("Token refresh error", error);
+            useAuthStore.getState().logoutFromStore();
+            return Promise.reject(error);
+        }
+    }
+
+    return Promise.reject(error);
 })
 
 export default api;
